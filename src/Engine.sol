4a915d59f3023ff78ff550ecd20790ee9a441fef
Joe Marron <marronjo@tcd.ie>
2023-11-08 21:44:56 +0000

    create engine skeleton contract

---
+++ src/Engine.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title Stabelcoin Algorithm Engine
+ * @author Joe Marron
+ * 
+ * This engine is designed to keep the stabelcoin at the price of 1$ 
+ */
+contract Engine {
+
+}
