ebef1a205e812e79b190dd8231c9a97ed8ff2152
Joe Marron <marronjo@tcd.ie>
2024-01-16 19:56:47 +0000

    add stablecoin fuzz tests

--- test/Stablecoin.t.sol
+++ test/Stablecoin.t.sol
@@ -19,6 +19,11 @@ contract StablecoinTest is Test {
         _;
     }
 
+    modifier assumeGreaterThanZero(uint256 amount){
+        vm.assume(amount > 0);
+        _;
+    }
+
     function setUp() public {
         vm.startBroadcast(owner);
         stablecoin = new Stablecoin();
@@ -30,10 +35,21 @@ contract StablecoinTest is Test {
         assert(stablecoin.mint(user, 1000) == true);
     }
 
+    function testFuzz_successfulMint(uint256 amount) public assumeGreaterThanZero(amount) {
+        vm.prank(owner);
+        assert(stablecoin.mint(user, amount) == true);
+    }
+
     function test_zeroAddressMint() public {
         vm.prank(owner);
         vm.expectRevert(Stablecoin.Stablecoin__NotZeroAddress.selector);
         stablecoin.mint(address(0), 5000);
+    }
+
+    function testFuzz_zeroAddressMint(uint256 amount) public assumeGreaterThanZero(amount) {
+        vm.prank(owner);
+        vm.expectRevert(Stablecoin.Stablecoin__NotZeroAddress.selector);
+        stablecoin.mint(address(0), amount);
     }
 
     function test_zeroAmountMint() public {
@@ -47,6 +63,11 @@ contract StablecoinTest is Test {
         stablecoin.burn(100);
     }
 
+    function testFuzz_successfulBurn(uint256 mintAmount) public assumeGreaterThanZero(mintAmount) mintTokensForOwner(mintAmount) {
+        vm.prank(owner);
+        stablecoin.burn(mintAmount);
+    }
+
     function test_burnZeroAmount() public mintTokensForOwner(100) {
         vm.prank(owner);
         vm.expectRevert(Stablecoin.Stablecoin__AmountMustBeGreaterThanZero.selector);
@@ -57,5 +78,12 @@ contract StablecoinTest is Test {
         vm.prank(owner);
         vm.expectRevert(Stablecoin.Stablecoin__BurnAmountExceedsBalance.selector);
         stablecoin.burn(600);
+    }
+
+    function testFuzz_burnExceedsBalance(uint256 mintAmount, uint256 burnAmount) public assumeGreaterThanZero(mintAmount) mintTokensForOwner(mintAmount) {
+        vm.assume(mintAmount < burnAmount);
+        vm.prank(owner);
+        vm.expectRevert(Stablecoin.Stablecoin__BurnAmountExceedsBalance.selector);
+        stablecoin.burn(burnAmount);
     }
 }
