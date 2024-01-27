8e092d0a35805c5da78981f77e41035797919409
Joe Marron <marronjo@tcd.ie>
2024-01-27 17:14:08 +0000

    complete interface method parameters

--- src/IEngine.sol
+++ src/IEngine.sol
@@ -4,16 +4,20 @@ pragma solidity ^0.8.18;
 
 interface IEngine {
 
-    function MintStablecoin(address collateralToken, uint256 collateralAmount) external; 
+    function MintStablecoin(address collateralToken, uint256 collateralAmount, uint256 mintAmount) external; 
 
     function depositCollateral(address collateralToken, uint256 collateralAmount) external;
 
+    function redeemCollateralForStablecoin(address collateralToken, uint256 collateralAmount, uint256 stablecoinAmount) external;
+
     function redeemCollateral(address collateralToken, uint256 collateralAmount) external;
 
-    function burnStablecoin() external;
+    function burnStablecoin(uint256 amount) external;
     
-    function liquidate() external;
+    function liquidateEntirePosition(address user) external;
+
+    function liquidatePartialPosition(address user, address collateralToken, uint256 amountToCover) external;
     
-    function getHealthFactor() external view;
-    
+    function getHealthFactor(address user) external view;
+
 }
