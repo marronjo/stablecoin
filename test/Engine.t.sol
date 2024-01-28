a675f8df3877ecf7f32621af63bf8e559996a790
Joe Marron <marronjo@tcd.ie>
2024-01-28 18:51:06 +0000

    happy path stablecoin minting test

--- test/Engine.t.sol
+++ test/Engine.t.sol
@@ -7,28 +7,49 @@ import {Vm} from "forge-std/Vm.sol";
 import {Engine} from "../src/Engine.sol";
 import {Stablecoin} from "../src/Stablecoin.sol";
 import {TokenMock} from "./mock/TokenMock.sol";
+import {AggregatorV3Mock} from "./mock/AggregatorV3Mock.sol";
 
 contract EngineTest is Test {
 
     Engine private engine;
     Stablecoin private stablecoin;
     TokenMock private collateralTokenMock;
+    AggregatorV3Mock private aggregatorV3Mock;
 
     address private owner = makeAddr("owner");
     address private user = makeAddr("user");
 
     function setUp() public {
         vm.startBroadcast(owner);
-        collateralTokenMock = new TokenMock();
         stablecoin = new Stablecoin();
         engine = new Engine(address(stablecoin));
 
+        collateralTokenMock = new TokenMock();
+        aggregatorV3Mock = new AggregatorV3Mock(1000);
+
+        //allow engine to mint new coins on stablecoin contract
         stablecoin.transferOwnership(address(engine));
         vm.stopBroadcast();
     }
 
-    function testFuzz_mintCorrectStablecoinAmount(uint256 collateralAmount, uint256 mintAmount) public {
-        vm.assume(mintAmount > 0 && collateralAmount > 0);
+    // function testFuzz_mintStablecoinNoCollateral(uint256 collateralAmount, uint256 mintAmount) public {
+    //     vm.assume(mintAmount > 0 && collateralAmount > 0);
+    //     vm.prank(user);
+    //     engine.mintStablecoin(address(collateralTokenMock), collateralAmount, mintAmount);
+    //     assertEq(mintAmount, engine.getUserStablecoinPosition(user));
+    // }
+
+    function test_sucessfulMintHealthyPosition() public {
+        //token mock price = 1000 USD
+        aggregatorV3Mock.setLatestPrice(1000);
+        engine.addAllowListedToken(address(collateralTokenMock), address(aggregatorV3Mock));
+        
+        // 1 token = 1000 USD total colateral
+        uint256 collateralAmount = 1;
+
+        // mint 700 tokens = 70% LTV ratio
+        uint256 mintAmount = 700; 
+
         vm.prank(user);
         engine.mintStablecoin(address(collateralTokenMock), collateralAmount, mintAmount);
         assertEq(mintAmount, engine.getUserStablecoinPosition(user));
