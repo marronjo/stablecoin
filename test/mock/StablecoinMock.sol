ed0d41492cee3ea7950d44fb7b0713191289debc
Joe Marron <marronjo@tcd.ie>
2024-01-29 21:21:45 +0000

    add stablecoin mock to simulate failed minting

---
+++ test/mock/StablecoinMock.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Stablecoin
+ * @author Joe Marron
+ * Algorithmic stabelcoin with crypto as collateral
+ * 
+ * ERC20 implementation of the stablecoin, no logic within this contract
+ */
+contract StablecoinMock is ERC20Burnable, Ownable { 
+
+    error Stablecoin__AmountMustBeGreaterThanZero();
+    error Stablecoin__BurnAmountExceedsBalance();
+    error Stablecoin__NotZeroAddress();
+    error Stablecoin__MintError();
+    
+    constructor() ERC20("Stablecoin", "STB") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if(_amount <= 0) {
+            revert Stablecoin__AmountMustBeGreaterThanZero();
+        }
+        if(balance < _amount) {
+            revert Stablecoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns(bool) {
+        return false;
+    }
+}
